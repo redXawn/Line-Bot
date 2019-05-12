306e7a77cb6111ccf9856676d1e7440c82e0cf45
andrew <sbstn.andrew56@gmail.com>
2019-05-12 13:39:27 +0700

    .

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const line = require('@line/bot-sdk');
 const port = process.env.PORT || 3000;
 const app = express();
 
-// app.use(logger('dev'));
+app.use(logger('dev'));
 // env.config({path: envPath})
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
