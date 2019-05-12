c2d4be38b83b4dfa7ad8d7a1fbd3f37916f04602
andrew <sbstn.andrew56@gmail.com>
2019-05-12 16:34:15 +0700

    .

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 'use strict';
 
-const line = require('@line/bot-sdk');
 const express = require('express');
+const bodyParser = require('body-parser');
+const line = require('@line/bot-sdk');
+const middleware = require('@line/bot-sdk').middleware
 
 const config = {
   channelAccessToken: '6JMxJk3lxVroozKbELsTnlkECPFgH+STEYuZCL7Yec/Q8Uj8XljPk33mTDfypMyoB0S1m5a2AAbOVt7XJAc1xxx9eoV6utk6onAGUArsBqWirt7OPLgrue+NJXx5UBaeKSYoPhESe/+lnQ3tU/eaVgdB04t89/1O/w1cDnyilFU=',
@@ -9,26 +11,30 @@ const config = {
 };
 
 const app = express();
+app.use(bodyParser.json())
+// app.use('/callback', middleware(config))
 
 app.post('/callback', (req, res) => {
+  console.log('req.body', req.body)
   Promise
     .all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
     .catch((err) => {
       console.error(err);
-      res.status(500).end();
+      res.status(500).send({message: 'Failed'});
     });
 });
 
 const client = new line.Client(config);
 function handleEvent(event) {
+  console.log('event', event.type, event.message.type)
   if (event.type !== 'message' || event.message.type !== 'text') {
     return Promise.resolve(null);
   }
 
   return client.replyMessage(event.replyToken, {
     type: 'text',
-    text: event.message.text
+    text: 'asd'
   });
 }
 
