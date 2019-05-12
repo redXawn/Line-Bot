b6d12567eeb7c53174ebc502c5fc291496e91287
andrew <sbstn.andrew56@gmail.com>
2019-05-12 14:27:38 +0700

    add secret

--- app.js
+++ app.js
@@ -1,80 +1,49 @@
-const express = require('express');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-const env = require('dotenv');
-const path = require('path');
+'use strict';
+
 const line = require('@line/bot-sdk');
+const express = require('express');
 
-const port = process.env.PORT || 3000;
-const app = express();
-
-const nodeEnv = process.env.NODE_ENV;
-let envPath
-if (nodeEnv === 'development') {
-  envPath = path.resolve('.env.development');
-} else if (nodeEnv === 'test') {
-  envPath = path.resolve('.env.test');
-} else if (nodeEnv === 'production') {
-  envPath = path.resolve('.env.production');
-}
-
-env.config({path: envPath})
-
-
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
+// create LINE SDK config from env variables
 const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: '6yCTovUsWXRUOm0/3ftFKgXNDmu+7jIy/dT8j+XfiIHBO+6V5JBX1gBootjRC3oo9YuyqsBeaEOUD75HNxWYJcHqTw8fGWGAtIybP2Jy+NtSPoe2bfw0TAv8/gmYVhMBl+Nh+Wa7khE9cq3U0zt0ZQdB04t89/1O/w1cDnyilFU=',
+  channelSecret: 'aa2ab409e4a95fcd8b8c82d6a066d867',
 };
 
+// create LINE SDK client
 const client = new line.Client(config);
 
-app.post('/webhook', line.middleware(config), (req, res) => {
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
   Promise
-      .all(req.body.events.map(handleEvent))
-      .then((result) => res.json(result))
-      .catch((err) => {
-    console.error(err);
-    res.status(500).end();
-  });
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
 });
 
-  function handleEvent(event) {
-    if (event.type !== 'message' || event.message.type !== 'text') {
-      // ignore non-text-message event
-      return Promise.resolve(null);
-    }
-    var options = {
-      method: 'GET',
-      url: 'https://api.susi.ai/susi/chat.json',
-      qs: {
-        timezoneOffset: '-330',
-        q: event.message.text
-    }
-  };
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
 
-  request(options, function(error, response, body) {
-    if (error) throw new Error(error);
-    // answer fetched from susi
-    var ans = (JSON.parse(body)).answers[0].actions[0].expression;
-    // create a echoing text message
-    const answer = {
-        type: 'text',
-        text: ans
-    };
-    // use reply API
-    return client.replyMessage(event.replyToken, answer);
-})
+  // create a echoing text message
+  const echo = { type: 'text', text: event.message.text };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
 }
 
-app.get('/', function (req, res) {
-  res.send({message: `Welcome`});
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
 });
-app.listen(port, function () {
-  console.log(`Example app listening on port ${port}!`);
-});
-
-module.exports = app
