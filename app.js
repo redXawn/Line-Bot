51195b78b2c68796243d7e8defb8db29afca3262
andrew <sbstn.andrew56@gmail.com>
2019-05-12 13:35:42 +0700

    install line sdk

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const env = require('dotenv');
+const line = require('@line/bot-sdk');
+
 const port = process.env.PORT || 3000;
 const app = express();
+
+app.use(logger('dev'));
+env.config({path: envPath})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', function (req, res) {
   res.send({message: 'Welcome'});
 });
