c7ecb2074a1520a9289e09275c279e40f2483876
andrew <sbstn.andrew56@gmail.com>
2019-05-12 12:47:53 +0700

    create develop

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const port = process.env.PORT || 3000;
+const app = express();
+app.get('/', function (req, res) {
+  res.send({message: 'Welcome'});
+});
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
