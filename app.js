a188ccbfca35d469c82aa5b6945d053f81342aca
andrew <sbstn.andrew56@gmail.com>
2019-05-12 12:19:54 +0700

    testing bot

--- app.js
+++ app.js
@@ -5,11 +5,7 @@ const path = require('path');
 const env = require('dotenv');
 const line = require('@line/bot-sdk');
 
-// Set up the express app
 const app = express();
-
-// Routes
-// const line = require('./routes/line-bot')
 
 const nodeEnv = process.env.NODE_ENV;
 let envPath
@@ -21,11 +17,6 @@ if (nodeEnv === 'development') {
   envPath = path.resolve('.env.production');
 }
 
-const config = {
-  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.LINE_CHANNEL_SECRET
-};
-
 // Log requests to the console.
 app.use(logger('dev'));
 env.config({path: envPath})
@@ -34,15 +25,29 @@ env.config({path: envPath})
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// app.use('/', line);
-
-app.use(function(req, res, next) {
-  res.status(404).send({message: 'Not Found'})
-});
+const config = {
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET
+};
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 app.get('/', (req, res) => res.status(200).send({
   message: 'Line Bot Example',
 }));
+
+app.post('/webhook', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
+
+app.use(function(req, res, next) {
+  res.status(404).send({message: 'Not Found'})
+});
 
 module.exports = app;
