a1ea972dd933743e0f19da0d1ea5ec63504c1b45
Sebastian Andrew <sebastian.andrew@ovo.id>
2019-06-10 19:15:52 +0700

    create dummy app

--- routes/line-bot.js
+++ routes/line-bot.js
@@ -3,7 +3,7 @@ const lineBotController = require('../controller/line-bot');
 
 router.post('/message/:id', lineBotController.pushMessage)
 router.get('/user/:id', lineBotController.getUserId)
-router.get('/user/all', lineBotController.tes)
-router.get('/user/tes', lineBotController.tes2)
+router.get('/userAll', lineBotController.tes)
+router.get('/tes1', lineBotController.tes2)
 
 module.exports = router;
