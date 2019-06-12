252923715c9902bea97ef1bdb2df4d6c68a6bfc0
Sebastian Andrew <sebastian.andrew@ovo.id>
2019-06-12 18:15:06 +0700

    add cron job

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const CronJob = require('cron').CronJob;
 require('dotenv').config()
 
 const app = express();
@@ -21,6 +22,39 @@ app.use('/callback', callbackRoute)
 app.get('/', (req, res) => res.status(200).send({
   message: 'Line Bot Example',
 }));
+
+// const { success, notFound, failed } = require('./utils/response');
+// const { findUser } = require('./utils/models/user');
+// const { pushMessageApi } = require('./services/line-services');
+
+// new CronJob('*/2 * * * *', async function(req, res) {
+//   try {
+//     console.log('send hi')
+//     const userData = await findUser('id', 1)
+//     if (!userData) {
+//       return notFound(req, res, null)
+//     }
+//     const body = {
+//       to: userData.line_user_id,
+//       messages:[
+//         {
+//           "type":"text",
+//           "text": "Hi"
+//         }
+//       ]
+//     }
+//     const response = await pushMessageApi(body)
+//     console.log('response', response.config.data)
+//     // res.send({message: 'ok'})
+//     // success(req, res, response.config.data)
+//   } catch(error) {
+//     failed(req, res, error)
+//   }
+// }, null, true, 'Asia/Bangkok')
+
+// new CronJob('*/2 * * * *', function() {
+//   console.log('tes cron')
+// }, null, true, 'Asia/Bangkok')
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
