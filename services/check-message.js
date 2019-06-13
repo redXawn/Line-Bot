3bfe076f75eb6d39adba9679ba90fb8a5a4ade0e
Sebastian Andrew <sebastian.andrew@ovo.id>
2019-06-14 00:00:33 +0700

    create new reply message for check koin price

--- services/check-message.js
+++ services/check-message.js
@@ -1,8 +1,10 @@
 const base64 = require('base-64');
 const moment = require('moment');
 const user = require('../server/models').user;
+const bitcoin = require('../server/models').bitcoin
 const { findUser } = require('../utils/models/user');
 const { pushMessageApi, replyMessageApi } = require('../services/line-services');
+const { getCoin } = require('../services/indodax-services')
 const { success, notFound, failed } = require('../utils/response');
 
 module.exports = {
@@ -71,7 +73,7 @@ module.exports = {
         response = await replyMessageApi(body)
         success(req, res, 'sucess')
       }
-    }  else if (actionType === 'resetpassword') {
+    } else if (actionType === 'resetpassword') {
       console.log(' reset asdsadsa')
       const randomPassword = Math.floor(Math.random() * 100000)
       await user.update({
@@ -92,6 +94,66 @@ module.exports = {
       }
       response = await replyMessageApi(body)
       success(req, res, 'sucess')
+    } else if (actionType === 'help' && userData.cookies) {
+      const body = {
+        to: lineId,
+        messages:[
+          {
+            "type":"text",
+            "text": 'Ketik "koin (kode koin)" untuk melihat harga koin'
+          },
+          {
+            "type":"text",
+            "text": 'ex koin btc'
+          }
+        ]
+      }
+      response = await pushMessageApi(body)
+      success(req, res, response.data)
+    } else if (actionType === 'koin' && userData.cookies) {
+      const coin = splitMessage[1]
+      const bitcoinResponse = await bitcoin.findOne({
+        where: { bitcoin_code: coin.toUpperCase() }
+      })
+      if (!bitcoinResponse) {
+        const body = {
+          to: lineId,
+          messages:[
+            {
+              "type":"text",
+              "text": `Koin yang kamu cari tidak ditemukkan`
+            }
+          ]
+        }
+        response = await pushMessageApi(body)
+        return notFound(req, res, null)
+      } else {
+        const bitcoin = await getCoin(bitcoinResponse.bitcoin_code)
+        const lastPrice = parseInt(bitcoin.data.ticker.last)
+        const body = {
+          to: lineId,
+          messages:[
+            {
+              "type":"text",
+              "text": `Harga ${bitcoinResponse.bitcoin_name} sekarang adalah Rp${lastPrice.toLocaleString(['ban', 'id'])}`
+            }
+          ]
+        }
+        response = await pushMessageApi(body)
+        success(req, res, response.data)
+      }
+    } else if (actionType === 'koin' && !userData.cookies) {
+      const body = {
+        to: lineId,
+        messages:[
+          {
+            "type":"text",
+            "text": "Anda Belum Login, sehingga tidak bisa menggunakan service ini"
+          }
+        ]
+      }
+      response = await pushMessageApi(body)
+      success(req, res, response.data)
     } else {
       const body = {
         replyToken,
