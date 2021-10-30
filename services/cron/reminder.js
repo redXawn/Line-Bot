3352b19d37deb7d6b94bf3a7fb0552506b9bc595
Sebastian Andrew <sebastian.andre@ovo.id>
2021-10-30 07:22:50 +0700

    new cron for reminder message

--- services/cron/reminder.js
+++ services/cron/reminder.js
@@ -70,6 +70,66 @@ module.exports = {
       'Asia/Bangkok'
     );
   },
+  reminderMessageV2() {
+    new CronJob(
+      '*/3 * * * *',
+      async function () {
+        try {
+          const reminderData = await reminder.findAll({
+            include: [
+              {
+                model: bitcoin,
+                attributes: ['bitcoin_code'],
+              },
+              {
+                model: user,
+                attributes: ['line_user_id'],
+              },
+            ],
+          });
+          reminderData.map(async (data) => {
+            const bitcoinResponse = await getCoin(data.bitcoin.bitcoin_code);
+            const lastPrice = parseInt(bitcoinResponse.data.ticker.last);
+            if (lastPrice <= data.reminder_price && data.active === true) {
+              const body = {
+                to: data.user.line_user_id,
+                messages: [
+                  {
+                    type: 'text',
+                    text: `Harga koin ${
+                      data.bitcoin.bitcoin_code
+                    } sudah dibawah / sama dengan ${data.reminder_price.toLocaleString([
+                      'ban',
+                      'id',
+                    ])}, harga sekarang adalah ${lastPrice.toLocaleString(['ban', 'id'])}`,
+                  },
+                ],
+              };
+              pushMessageApi(body);
+            } else {
+              return;
+            }
+          });
+        } catch (error) {
+          console.log(error);
+          const body = {
+            to: userData.line_user_id,
+            messages: [
+              {
+                type: 'text',
+                text: 'Terjadi kesalahan pada reminder servis',
+              },
+            ],
+          };
+          pushMessageApi(body);
+          failed(req, res, 'Error reminder');
+        }
+      },
+      null,
+      true,
+      'Asia/Bangkok'
+    );
+  },
   // genshinDaily1() {
   //   new CronJob(
   //     '0 */12 * * *',
